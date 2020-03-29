85e9bb33ac6393e4f55c09f850f5a897f270d3cf
Diogo Almeida <Diogo.Almeida@ctw.bmwgroup.com>
2020-03-29 15:45:46 +0100

    * Added global CORS configuration

---
+++ src/main/java/com/vejo/em/casa/be/BeConfiguration.java
@@ -0,0 +1,20 @@
+package com.vejo.em.casa.be;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class BeConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
